2055ea3ee00e79e83b419b270cb419f5045d5117
sok-leaphea <leapheas2@gmail.com>
2024-12-16 16:37:41 +0700

    Add project files.

---
+++ MyApp/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var item = new Item() { Name = "Keyboard" };
+            return View(item);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+    }
+}
---
+++ MyApp/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace MyApp.Models
+{
+    public class Item
+    {
+        public int ID {  get; set; }
+        public string Name { get; set; }
+
+    }
+
+}
